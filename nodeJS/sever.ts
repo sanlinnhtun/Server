da9039a07ef3173c393879a75b4ae4ea8ab63947
sanlinnhtun <lsanlynnh@gmail.com>
2024-08-12 16:44:45 +0630

    node

---
+++ nodeJS/sever.ts
@@ -0,0 +1,40 @@
+import http, { IncomingMessage, ServerResponse } from 'http';
+import fs from 'fs';
+import { Menu, MenuCategories } from './type';
+
+const PORT = 3000;
+
+const menus: Menu[] = [];
+const menuCategories: MenuCategories[] = [];
+
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+      const data = fs.readFileSync('index.html');
+      res.write(data);
+      res.end();
+    } else if (url === '/script.js') {
+      const data = fs.readFileSync('script.js');
+      res.write(data);
+      res.end();
+    } else if (url === '/menu') {
+      switch (method) {
+        case 'GET':
+          res.write(JSON.stringify(menus));
+          res.end();
+          break;
+        case 'POST':
+          break;
+        case 'PUT':
+          break;
+        case 'DELETE':
+          break;
+      }
+    } else if (url === '/menu-categories') {
+    }
+  }
+);
+
+server.listen(PORT, () => console.log(`Server is listen on ${PORT}`));
---
+++ nodeJS/type.ts
@@ -0,0 +1,11 @@
+export interface Menu {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export interface MenuCategories {
+  id: number;
+  name: string;
+  isAvailabel: boolean;
+}
